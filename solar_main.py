3f8dc0516a13ad0e0f0cd69be155a8a37881a171
Krivchuk Vasily <krivchuk.vo@phystech.edu>
2021-11-12 16:13:08 +0300

    Добавлены функционирующие кнопки

--- solar_main.py
+++ solar_main.py
@@ -44,7 +44,7 @@ def write_to_written():
     pass
 
 def start_execution():
-    """Обработчик события нажатия на кнопку Start.
+    """Обработчик события нажатия на кнопку Play.
     Запускает циклическое исполнение функции execution.
     """
     global perform_execution
@@ -57,14 +57,14 @@ def pause_execution():
 
 
 def stop_execution():
-    """Обработчик события нажатия на кнопку Start.
+    """Обработчик события нажатия на кнопку Pause.
     Останавливает циклическое исполнение функции execution.
     """
     global alive
     alive = False
 
 
-def open_file():
+def open_file1():
     """Открывает диалоговое окно выбора имени файла и вызывает
     функцию считывания параметров системы небесных тел из данного файла.
     Считанные объекты сохраняются в глобальный список space_objects
@@ -74,11 +74,40 @@ def open_file():
     global model_time
 
     model_time = 0.0
-    in_filename = "solar_system.txt"
+    in_filename = 'solar_system.txt'
     space_objects = read_space_objects_data_from_file(in_filename)
     max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
 
+def open_file2():
+    """Открывает диалоговое окно выбора имени файла и вызывает
+    функцию считывания параметров системы небесных тел из данного файла.
+    Считанные объекты сохраняются в глобальный список space_objects
+    """
+    global space_objects
+    global browser
+    global model_time
+
+    model_time = 0.0
+    in_filename = 'double_star.txt'
+    space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
+    calculate_scale_factor(max_distance)
+
+def open_file3():
+    """Открывает диалоговое окно выбора имени файла и вызывает
+    функцию считывания параметров системы небесных тел из данного файла.
+    Считанные объекты сохраняются в глобальный список space_objects
+    """
+    global space_objects
+    global browser
+    global model_time
+
+    model_time = 0.0
+    in_filename = 'written.txt'
+    space_objects = read_space_objects_data_from_file(in_filename)
+    max_distance = max([max(abs(obj.x), abs(obj.y)) for obj in space_objects])
+    calculate_scale_factor(max_distance)
 
 def handle_events(events, menu):
     global alive
@@ -96,48 +125,57 @@ def slider_reaction(event):
     global time_scale
     time_scale = slider_to_real(event.el.get_value())
 
+def test():
+    print('Test')
 
 def init_ui(screen):
     global browser
-    slider = thorpy.SliderX(100, (-20, 20), "Simulation speed")
+    slider = thorpy.SliderX(100, (-12, 12), "Simulation speed")
     slider.user_func = slider_reaction
     button_stop = thorpy.make_button("Quit", func=stop_execution)
     button_pause = thorpy.make_button("Pause", func=pause_execution)
     button_play = thorpy.make_button("Play", func=start_execution)
+    button_load1 = thorpy.make_button("Solar system", func=open_file1)
+    button_load2 = thorpy.make_button("Double_star", func=open_file2)
+    button_load3 = thorpy.make_button("Load save", func=open_file3)
     timer = thorpy.OneLineText("Seconds passed")
 
-    button_write = thorpy.make_button("Safe data",  write_space_objects_data_to_file("written.txt", space_objects))
-    button_load = thorpy.make_button(text="Load a file", func=open_file)
+    button_write = thorpy.make_button("Save data", func=write_space_objects_data_to_file("written.txt", space_objects))
+    button_load = thorpy.make_button(text="Load a file", func=open_file1)
 
     box = thorpy.Box(elements=[
         slider,
-        button_pause, 
-        button_stop, 
-        button_play, 
+        button_pause,
+        button_stop,
+        button_play,
         button_load,
         button_write,
+        button_load1,
+        button_load2,
+        button_load3,
         timer])
     reaction1 = thorpy.Reaction(reacts_to=thorpy.constants.THORPY_EVENT,
                                 reac_func=slider_reaction,
-                                event_args={"id":thorpy.constants.EVENT_SLIDE},
+                                event_args={"id": thorpy.constants.EVENT_SLIDE},
                                 params={},
                                 reac_name="slider reaction")
     box.add_reaction(reaction1)
-    
+
     menu = thorpy.Menu(box)
     for element in menu.get_population():
         element.surface = screen
 
-    box.set_topleft((100,100))
+    box.set_topleft((100, 100))
     box.blit()
     box.update()
     return menu, box, timer
+
 
 def main():
     """Главная функция главного модуля.
     Создаёт объекты графического дизайна библиотеки tkinter: окно, холст, фрейм с кнопками, кнопки.
     """
-    
+
     global physical_time
     global displayed_time
     global time_step
@@ -151,29 +189,24 @@ def main():
     physical_time = 0
 
     pg.init()
-    
+
     width = 1000
     height = 900
     screen = pg.display.set_mode((width, height))
     last_time = time.perf_counter()
     drawer = Drawer(screen)
     menu, box, timer = init_ui(screen)
-    perform_execution = True
 
     while alive:
-        write_space_objects_data_to_file("written.txt", space_objects)
         handle_events(pg.event.get(), menu)
         cur_time = time.perf_counter()
         if perform_execution:
             execution((cur_time - last_time) * time_scale)
             text = "%d seconds passed" % (int(model_time))
             timer.set_text(text)
-            write_space_objects_data_to_file("written.txt", space_objects)
-
         last_time = cur_time
         drawer.update(space_objects, box)
         time.sleep(1.0 / 60)
-    print(space_objects)
     print('Modelling finished!')
 
 
